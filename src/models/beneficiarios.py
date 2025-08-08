e934f92f857c4564a579d353df1a7d8b894e9f62
noreply-cecapmex <baguirre@cecapmex.com>
2025-08-08 09:57:19 -0600

    [PPABE][BACKEND] -> Creación Modelo de Beneficiarios

---
+++ src/models/beneficiarios.py
@@ -0,0 +1,20 @@
+from ..database.connection import db
+
+class Beneficiarios(db.Model):
+    __tablename__ = 'beneficiarios'
+    
+    id              = db.Column(db.Integer, primary_key = True)
+    CURP            = db.Column(db.String(18), unique=True, nullable=False)
+    RFC	            = db.Column(db.String(13), unique=True, nullable=False)
+    nombre	        = db.Column(db.String(255))
+    aPaterno        = db.Column(db.String(255))
+    aMaterno        = db.Column(db.String(255))
+    fNacimiento	    = db.Column(db.Date)
+    creador	        = db.Column(db.String(36))
+    modificador	    = db.Column(db.String(36))
+    fCreacion	    = db.Column(db.Date)
+    fModificacion   = db.Column(db.Date)
+    deleted         = db.Column(db.Boolean)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
