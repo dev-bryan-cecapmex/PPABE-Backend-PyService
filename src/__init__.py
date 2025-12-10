8118b1edfb9dfdfbd4b507ba13975ec91cf06e57
baguirre-cecapmex <baguirre@cecapmex.com>
2025-12-10 11:32:23 -0600

    [PPABE][BACKEND] -> Actualización de cache cuando se levanta el servicio

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,8 @@ from .routes.v1.root import root_bp
 from .routes.v1.public import public_bp
 from .routes.v1.private import private_bp
 
+# Importacion de servicio de refresco de cache 
+from src.services.search_service import SearchService
 
 def create_app():
     app = Flask(__name__)
@@ -36,6 +38,14 @@ def create_app():
     app.register_blueprint(root_bp)
     app.register_blueprint(public_bp, url_prefix="/api/v1/public")
     app.register_blueprint(private_bp, url_prefix="/api/v1/private")
+    
+    with app.app_context():
+        try:
+            print("⚡ Inicializando caché de catálogos...")
+            SearchService.force_refresh_cache()
+            print("✔ Caché inicial cargada correctamente.")
+        except Exception as ex:
+            print("❌ ERROR cargando caché inicial:", ex)
 
     return app
 
